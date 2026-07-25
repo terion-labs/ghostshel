5eb33426c30b0a58879ba1a434594d298f81bbd2
terion <mail@terion.name>
2026-07-25 14:01:31 +0300

    Extract passive runtime panel views

---
+++ src/GhostShell.App/Views/RuntimePanels/ProcessMonitorRuntimePanelView.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace GhostShell.App.Views.RuntimePanels;
+
+public sealed partial class ProcessMonitorRuntimePanelView : UserControl
+{
+    public ProcessMonitorRuntimePanelView()
+    {
+        InitializeComponent();
+    }
+
+    public event EventHandler<RoutedEventArgs>? CloseRequested;
+
+    private void OnCloseClick(object? sender, RoutedEventArgs e)
+    {
+        _ = sender;
+        CloseRequested?.Invoke(this, e);
+    }
+}
